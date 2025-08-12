b22ae6f51fab3ff6afc197429b9e14c163340734
Kushal Niraula <kushal463334@gmail.com>
2025-08-13 08:50:00 +1000

    Create User DTOs

---
+++ SplitSmart.API/DTOs/UserDTOs.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SplitSmart.API.DTOs
+{
+    public class UserProfileDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class UpdateUserDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+    }
+
+    public class ChangePasswordDto
+    {
+        [Required]
+        public string CurrentPassword { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
