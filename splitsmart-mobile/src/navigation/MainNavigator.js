f4331c37e87ffb676509daee737702c3ec8b5087
Kushal Niraula <kushal463334@gmail.com>
2025-10-05 14:23:00 +1000

    Update Main navigator with all screens

--- splitsmart-mobile/src/navigation/MainNavigator.js
+++ splitsmart-mobile/src/navigation/MainNavigator.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
+import CreateGroupScreen from "../screens/CreateGroupScreen";
+import GroupDetailScreen from "../screens/GroupDetailScreen";
+import AddExpenseScreen from "../screens/AddExpenseScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +15,9 @@ export default function MainNavigator() {
       }}
     >
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="CreateGroup" component={CreateGroupScreen} />
+      <Stack.Screen name="GroupDetail" component={GroupDetailScreen} />
+      <Stack.Screen name="AddExpense" component={AddExpenseScreen} />
     </Stack.Navigator>
   );
 }
