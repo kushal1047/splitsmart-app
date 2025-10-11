122831da357347edae684b039d896529289d6924
Kushal Niraula <kushal463334@gmail.com>
2025-10-11 16:32:00 +1000

    Add Settlements and Profile screens to Main navigator

--- splitsmart-mobile/src/navigation/MainNavigator.js
+++ splitsmart-mobile/src/navigation/MainNavigator.js
@@ -4,6 +4,8 @@ import HomeScreen from "../screens/HomeScreen";
 import CreateGroupScreen from "../screens/CreateGroupScreen";
 import GroupDetailScreen from "../screens/GroupDetailScreen";
 import AddExpenseScreen from "../screens/AddExpenseScreen";
+import SettlementsScreen from "../screens/SettlementsScreen";
+import ProfileScreen from "../screens/ProfileScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +20,8 @@ export default function MainNavigator() {
       <Stack.Screen name="CreateGroup" component={CreateGroupScreen} />
       <Stack.Screen name="GroupDetail" component={GroupDetailScreen} />
       <Stack.Screen name="AddExpense" component={AddExpenseScreen} />
+      <Stack.Screen name="Settlements" component={SettlementsScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
     </Stack.Navigator>
   );
 }
